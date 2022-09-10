d2bf06cd497ff29a0dd96619cfdca3ecf3a1fd74
DanielN24 <capdaniel_98@outlook.com>
2022-09-10 17:47:45 -0500

    new file with the answer number 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,19 +13,21 @@ env.hosts = ['34.203.31.100', '54.227.101.39']
 
 def do_deploy(archive_path):
     """Prototype: def do_deploy"""
-    
+
     if exists(archive_path) is False:
         return False
     archive = archive_path.split('/')[1]
     put("archive_path", "/tmp/")
     run("mkdir -p /data/web_static/releases/{}".format(archive[0:-4]))
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".fotmat(archive, archive[0:-4]))
+    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".fotmat(
+        archive, archive[0:-4]))
     run("mv /data/web_static/releases/{}/web_static/*\
-            /data/web_static/releases/{}/".format(archive[0:-4], archive[0:-4]))
+            /data/web_static/releases/{}/".format(
+                archive[0:-4], archive[0:-4]))
     run("rm -rf /tmp/{}".format(archive))
     run("rm -f /data/web_static/releases/{}/web_static".fotmat(archive))
     run("rm /data/web_static/current")
-    run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(archive[0:-4]))
+    run("ln -s /data/web_static/releases/{}/ \
+        /data/web_static/current".format(archive[0:-4]))
     sudo("service nginx restart")
     return True
- 
