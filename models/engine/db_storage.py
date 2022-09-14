bb4ae79c25f20c11aeffd1bf8f8a3e8b4954ca08
DanielN24 <capdaniel_98@outlook.com>
2022-09-14 14:37:57 -0500

    correction some files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -70,4 +70,4 @@ class DBStorage:
 
     def close(self):
         """Reload method"""
-        self.__session.remove()
+        self.__session.close()
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,13 +2,14 @@
 """ script that starts a Flask web application """
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def lists():
     """ list of states """
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 
